9c019021b33001fb45ceb5d3f07a23e3919f6283
yepeng(叶鹏) <yepeng@yiguo.com>
2018-08-20 10:03:05 +0800

    Topshelf

---
+++ 后端系列/Topshelf/Topshelf_Framework_Exe/MyService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Topshelf_Framework_Exe
+{
+    public class MyService : ServiceControl
+    {
+        private readonly Process _process;
+
+        public MyService()
+        {
+            _process = new Process();
+        }
+        public bool Start(HostControl hostControl)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory;// 当前运行程序的根路径
+                _process.StartInfo.FileName = path + "IntelliJIDEALicenseServer_windows_amd64.exe";
+                _process.StartInfo.WorkingDirectory = path;
+                _process.StartInfo.CreateNoWindow = true;//不在新窗口打开
+                _process.Start();
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            try
+            {
+                _process.Kill();
+            }
+            catch (Exception e)
+            {
+
+            }
+            return true;
+        }
+    }
+}
---
+++ 后端系列/Topshelf/Topshelf_Framework_Exe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Topshelf_Framework_Exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             安装：TopshelfDemo.exe install
+             启动：TopshelfDemo.exe start
+             卸载：TopshelfDemo.exe uninstall
+             */
+            try
+            {
+                HostFactory.Run(i =>
+                {
+                    i.Service<MyService>();
+
+                    i.RunAsLocalSystem();
+                    i.SetDescription("Resharper License 服务");
+                    i.SetServiceName("Resharper License");
+                    i.SetDisplayName("Resharper License");
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
