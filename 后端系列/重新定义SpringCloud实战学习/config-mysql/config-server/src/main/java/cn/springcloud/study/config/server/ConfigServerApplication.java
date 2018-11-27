43f937532c4c199f835218f51ef761bfd2e3be72
yepeng <yepeng@yiguo.com>
2018-11-27 19:46:31 +0800

    spring cloud config

---
+++ 后端系列/重新定义SpringCloud实战学习/config-mysql/config-server/src/main/java/cn/springcloud/study/config/server/ConfigServerApplication.java
@@ -0,0 +1,12 @@
+package cn.springcloud.study.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
