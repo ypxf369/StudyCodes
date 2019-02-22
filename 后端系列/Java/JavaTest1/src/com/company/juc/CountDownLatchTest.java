7374fc47ca3ee6ccaa6b64907c0661b5df75412b
yepeng <ypxf369@163.com>
2019-02-22 17:48:23 +0800

    juc

--- 后端系列/Java/JavaTest1/src/com/company/juc/CountDownLatchTest.java
+++ 后端系列/Java/JavaTest1/src/com/company/juc/CountDownLatchTest.java
@@ -1,18 +1,19 @@
 package com.company.juc;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * Created by yepeng on 2019/02/21.
+ *
+ * CountDownLatch 当指定线程数任务都完成时，在执行此任务
  */
 public class CountDownLatchTest {
     public static void main(String[] args) throws InterruptedException {
         final CountDownLatch _latch = new CountDownLatch(2);
+
         Thread t1 = new Thread(() -> {
             try {
                 System.out.println("子线程" + Thread.currentThread().getName() + "开始执行");
@@ -34,9 +35,14 @@ public class CountDownLatchTest {
                 e.printStackTrace();
             }
         });
-        Executor executorService = Executors.newFixedThreadPool(2);
-        executorService.execute(t1);
-        executorService.execute(t2);
+//        Executor executorService = Executors.newFixedThreadPool(2);
+//        executorService.execute(t1);
+//        executorService.execute(t2);
+
+        ThreadPoolExecutor poolExecutor=new ThreadPoolExecutor(2, 2, 2, TimeUnit.SECONDS,new LinkedBlockingQueue(2));
+        poolExecutor.execute(t1);
+        poolExecutor.execute(t2);
+
         System.out.println("等待两个线程执行完毕");
         _latch.await();
         System.out.println("两个子线程执行完毕");
