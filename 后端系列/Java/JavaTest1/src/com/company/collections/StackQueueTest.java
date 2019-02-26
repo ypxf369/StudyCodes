4e3437cfdd3149918be7c2bc460af36319b1f295
yepeng <ypxf369@163.com>
2019-02-26 19:52:19 +0800

    nio

---
+++ 后端系列/Java/JavaTest1/src/com/company/collections/StackQueueTest.java
@@ -0,0 +1,73 @@
+package com.company.collections;
+
+/**
+ * Created by yepeng on 2019/02/26.
+ */
+public class StackQueueTest {
+
+    class MyStack {
+
+        MyNode top;
+
+        public MyNode peek() {
+            if (top != null) {
+                return top;
+            }
+            return null;
+        }
+
+        public MyNode pop() {
+            if (top != null) {
+                MyNode temp = new MyNode(top.value);
+                top = top.next;
+                return temp;
+            }
+            return null;
+        }
+
+        public void push(MyNode value) {
+            if (value != null) {
+                //top.next = top;
+                //top = temp;
+                value.next = top;
+                top = value;
+            }
+        }
+    }
+
+    class MyQueue {
+        MyNode first, last;
+
+        public MyNode dequeue() {
+            if (first == null) {
+                return null;
+            } else {
+                MyNode temp = new MyNode(first.value);
+                first = first.next;
+                return temp;
+            }
+        }
+
+        public void enqueue(MyNode value) {
+            if (value != null) {
+                if (first == null) {
+                    first = value;
+                    last = value;
+                } else {
+                    last.next = value;
+                    last = value;
+                }
+            }
+        }
+    }
+
+    class MyNode {
+        public MyNode(Object value) {
+            this.value = value;
+            this.next = null;
+        }
+
+        public Object value;
+        public MyNode next;
+    }
+}
---
+++ 后端系列/Java/JavaTest1/src/com/company/nio/NIOTest.java
@@ -0,0 +1,58 @@
+package com.company.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+
+/**
+ * Created by yepeng on 2019/02/26.
+ */
+public class NIOTest {
+    private Charset charset = Charset.forName("UTF-8");
+
+    private SocketChannel channel;
+
+
+    private void readHtmlContent() {
+        try {
+            connect();
+            sendRequest();
+            readResponse();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (channel != null) {
+                try {
+                    channel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void connect() throws IOException {
+        InetSocketAddress socketAddress = new InetSocketAddress("www.csdn.net", 80);
+        channel = SocketChannel.open(socketAddress);
+        channel.configureBlocking(false);
+    }
+
+    private void sendRequest() throws IOException {
+        channel.write(charset.encode("GET"));
+    }
+
+    private void readResponse() throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);//创建缓冲区
+        while (channel.read(buffer) != -1) {
+            buffer.flip();
+            System.out.println(charset.decode(buffer));
+            buffer.clear();
+        }
+    }
+
+    public static void main(String[] args) {
+        new NIOTest().readHtmlContent();
+    }
+}
